3d9edecbf2df21b596e72d45d87f25c705447d1c
gilbertrendon <jilberlv@hotmail.com>
2025-03-17 18:25:32 -0500

    comienzo interfaz final

--- app/src/App.js
+++ app/src/App.js
@@ -1,49 +1,127 @@
+// import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
+// import './App.css'
+
+
+// function App() {
+//   const [message, setMessage] = useState('');
+//   const [data, setData] = useState('');
+
+
+//   useEffect(() => {
+//     async function fetchData() { 
+//       try {
+//         const response = await axios.get('http://localhost:3000/getData');
+//         setMessage(response.data.message);
+//       } catch (error) {
+//         console.log("****************ddddddddd*************************")
+
+//         console.error('Error al obtener los datos:', error);
+//         setMessage('Error al obtener los datos');
+//       }
+//     };
+
+//     async function fetchProducts() { 
+//       try {
+//         const response = await axios.get('http://localhost:3000/products');
+//         console.log("*********************rrrrrrrr********************")
+//         console.log(response.data[0])
+//         setData(response.data);
+//       } catch (error) {
+//         console.log("*****************************************")
+
+//         console.error('Error al obtener los datos:', error);
+//         setData('Error al obtener los datos');
+//       }
+//     };
+//     fetchProducts();
+//     fetchData();
+//   }, []);
+
+//   function Product({ product }) {
+//     return (
+//       <div className="product">
+//         <h3>{product.name}</h3>
+//         <p>{product.description}</p>
+//       </div>
+//     )
+//   }
+
+//   return (
+//     <div className="App">
+//       <h1>{message}</h1>
+//       <div className="products-grid">
+//         {products.map((product) => (
+//           <Product key={product.id} product={product} />
+//         ))}
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import './App.css';
 
 function App() {
   const [message, setMessage] = useState('');
-  const [data, setData] = useState('');
-
+  const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    async function fetchData() { // Se agrega el async a la función fetchData
-      try {
-        const response = await axios.get('http://localhost:3000/getData');
-        setMessage(response.data.message);
-      } catch (error) {
-        console.log("****************ddddddddd*************************")
-
-        console.error('Error al obtener los datos:', error);
-        setMessage('Error al obtener los datos');
-      }
-    };
-
-    async function fetchProducts() { // Se agrega el async a la función fetchData
-      try {
-        const response = await axios.get('http://localhost:3000/products');
-        console.log("*********************rrrrrrrr********************")
-        console.log(response.data[0])
-        setData(response.data[0]._id);
-      } catch (error) {
-        console.log("*****************************************")
-
-        console.error('Error al obtener los datos:', error);
-        setData('Error al obtener los datos');
-      }
-    };
     fetchProducts();
-    fetchData();
   }, []);
 
+  async function fetchProducts() {
+    setIsLoading(true)
+    try {
+      const response = await axios.get('http://localhost:3000/products');
+      console.log(response.data);
+      setProducts(response.data);
+    } catch (error) {
+      console.error('Error al obtener los datos:', error);
+      setMessage("Error al obtener los datos");
+    } finally {
+      setIsLoading(false)
+    }
+  };
 
+  function Product(props) {
+    const { product } = props;
+    return (
+      <div className="product">
+        <h2 className="product-name">{product.name}</h2>
+        <p className="product-description">{product.description}</p>
+      </div>
+    );
+  }
 
   return (
     <div className="App">
-      data{message}
-      products{data}
+      {message &&
+        <div className="error-message">{message}</div>
+      }
+      {isLoading ? (
+        <p>Cargando productos...</p>
+      ) : (
+        <div className="products-grid">
+          {products.map((product) => (
+            <div key={product.id} className="product">
+              <h2 className="product-name">{product.name}</h2>
+              <p className="product-description">{product.description}</p>
+              <button>Update</button>
+              <br></br>
+              <button style={{backgroundColor:'red'}}>Delete</button>
+            </div>
+          ))}
+
+        </div>
+
+      )}
+      <button style={{"margin-left":"auto","margin-right":"auto" ,  display:'block'}} >Create</button>`
     </div>
-   
   );
 }
 
