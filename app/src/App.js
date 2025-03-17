7636ce32eb1b4513aa97b8415927b5752dc0a325
gilbertrendon <jilberlv@hotmail.com>
2025-03-17 15:45:09 -0500

    llamado a obtener productos completado

--- app/src/App.js
+++ app/src/App.js
@@ -3,26 +3,47 @@ import axios from 'axios';
 
 function App() {
   const [message, setMessage] = useState('');
+  const [data, setData] = useState('');
+
 
   useEffect(() => {
     async function fetchData() { // Se agrega el async a la función fetchData
-        try {
+      try {
         const response = await axios.get('http://localhost:3000/getData');
-        setMessage(response.data.message); 
+        setMessage(response.data.message);
       } catch (error) {
+        console.log("****************ddddddddd*************************")
+
         console.error('Error al obtener los datos:', error);
         setMessage('Error al obtener los datos');
       }
     };
 
+    async function fetchProducts() { // Se agrega el async a la función fetchData
+      try {
+        const response = await axios.get('http://localhost:3000/products');
+        console.log("*********************rrrrrrrr********************")
+        console.log(response.data[0])
+        setData(response.data[0]._id);
+      } catch (error) {
+        console.log("*****************************************")
+
+        console.error('Error al obtener los datos:', error);
+        setData('Error al obtener los datos');
+      }
+    };
+    fetchProducts();
     fetchData();
   }, []);
 
 
+
   return (
     <div className="App">
-        {message}
+      data{message}
+      products{data}
     </div>
+   
   );
 }
 
